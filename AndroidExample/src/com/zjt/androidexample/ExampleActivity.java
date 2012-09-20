7f6cd6a539c2d84528838155da912e9172500fcd
zhangjieting <zhangjieting@gmail.com>
2012-09-20 10:33:57 +0800

    init AndroidExample and commit download function

---
+++ AndroidExample/src/com/zjt/androidexample/ExampleActivity.java
@@ -0,0 +1,42 @@
+package com.zjt.androidexample;
+
+import com.zjt.androidexample.activities.DownloadActivity;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class ExampleActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_example);
+        
+        setupViews();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_example, menu);
+        return true;
+    }
+    
+    private void setupViews() {
+    	Button button1 = (Button) findViewById(R.id.button1);
+    	button1.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				Intent intent = new Intent();
+				intent.setClass(getApplicationContext(), DownloadActivity.class);
+				startActivity(intent);
+			}
+    		
+    	});
+    }
+}
---
+++ AndroidExample/src/com/zjt/androidexample/activities/DownloadActivity.java
@@ -0,0 +1,63 @@
+package com.zjt.androidexample.activities;
+
+import com.zjt.androidexample.R;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.DownloadManager;
+import android.app.DownloadManager.Request;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class DownloadActivity extends Activity {
+	
+	private DownloadManager mDownloadManager = null;
+	private long lastDownload = -1L;
+	
+	private String strUrl="http://dl.google.com/android/ndk/android-ndk-r6-linux-x86.tar.bz2";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_download);
+		
+		setupViews();
+		mDownloadManager = (DownloadManager)getSystemService(DOWNLOAD_SERVICE);
+	}
+
+    private void setupViews() {
+    	Button button = (Button) findViewById(R.id.button_download);
+    	button.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				startDownload(strUrl);
+			}
+    		
+    	});
+    }
+
+	@SuppressLint("NewApi")
+	public void startDownload(String strUrl) { 
+        Uri uri=Uri.parse(strUrl); 
+ 
+        Environment 
+            .getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) 
+            .mkdirs(); 
+ 
+        Request req = new DownloadManager.Request(uri); 
+ 
+        req.setTitle("Demo"); 
+        req.setDescription("android-ndk-r6-linux-x86.tar.bz2"); 
+        req.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS,"android-ndk-r6-linux-x86.tar.bz2"); 
+        req.setNotificationVisibility(0); 
+        req.setShowRunningNotification(true); 
+ 
+        lastDownload = mDownloadManager.enqueue(req);  
+    }    
+	
+}
