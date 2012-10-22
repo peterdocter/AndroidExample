273a1d2cf27c02d28bc2f61934eba5febe066a3f
zhangjieting <zhangjieting@gmail.com>
2012-10-22 14:16:23 +0800

    add WebViewClient for WebViewActivity,在webview展示的页面点击新链接时，能够用该webview打开，而不是跳转到浏览器

--- AndroidExample/src/com/ting/androidexample/ui/activities/WebViewActivity.java
+++ AndroidExample/src/com/ting/androidexample/ui/activities/WebViewActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class WebViewActivity extends Activity {
 
@@ -12,11 +13,26 @@ public class WebViewActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		
 		WebView webView = new WebView(this);
-		webView.loadUrl("http://map.baidu.com/mobile");
+		webView.loadUrl("http://www.baidu.com");
+//		webView.loadUrl("http://map.baidu.com/mobile");
 		setContentView(webView);
+		
+		//在webview展示的页面点击新链接时，能够用该webview打开，而不是跳转到浏览器
+		webView.setWebViewClient(new WebViewClient() {
+			
+			@Override
+	         public boolean shouldOverrideUrlLoading(WebView view, String url) {
+	 
+				view.loadUrl(url);   //在当前的webview中跳转到新的url
+				return true;
+	         }
+		});
+		
 		WebSettings webSettings = webView.getSettings();
+		//enable application cache
 		webSettings.setCacheMode(WebSettings.LOAD_NORMAL);
 		webSettings.setAppCacheEnabled(true);
+		//enable javascript
 		webSettings.setJavaScriptEnabled(true);
 	}
 
